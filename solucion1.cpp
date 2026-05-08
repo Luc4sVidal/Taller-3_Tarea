de6fc09f0697c2c340a35b32ab644a767cab5348
conyta1256 <constanza.gallardo03@alumnos.uach.cl>
2026-05-07 23:49:09 -0400

    actualizado

--- solucion1.cpp
+++ solucion1.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+typedef unsigned char uchar;
+
+struct Vectores{
+    uchar** vec; //Arreglo dinamico del tipo uchar
+    int capacidad_max;
+    int tamaño;
+    float overhead; //porcentaje de celdas adicionales 
+};
+
+void insertar(Vectores &v, const char* nuevaPalabra){
+    uchar** nuevoVec;
+    //Redimensionar el vector una vez se haya alcanzado la capacidad maxima de claves 
+    if(v.tamaño == v.capacidad_max){ 
+        int nueva_cap = v.capacidad_max + (v.capacidad_max*v.overhead);
+        nuevoVec = new uchar*[nueva_cap];
+
+        for(int i = 0; i < v.tamaño; i++){
+            nuevoVec[i] = v.vec[i];
+        }
+
+        delete[] v.vec;
+        v.vec = nuevoVec;
+        v.capacidad_max = nueva_cap;
+    }
+
+}
 
 int main(){
+
     return 0;
 }
